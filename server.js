b79980276cbb55f452bcd34e18fe6538065ca0db
rohinthtruelineresearch-cloud <rohinthtruelineresearch@gmail.com>
2026-01-03 15:47:56 +0530

    Update CORS to allow truelineresearch.in

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://truelineresearch.in', 'http://localhost:3000'],
+  credentials: true
+}));
 app.use(bodyParser.json());
 
 // MongoDB Connection
